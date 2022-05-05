9f29a927a931c65fe9bfdcf23a7e6a3b1fb7e0bb
Steve Fitgerald <steven.fitzgerald@my.csun.edu>
2022-05-05 16:20:51 -0700

    Version 3
    modified the subroutine to have three input paramaters
    allocated a buffer via syscal #9
    modify the final loop to put values in a string buffer
      -- note added to add a null char at the end of the loop
    
    note bug exists if we execeed the buffer length
    
    lessons:
       - step on my registers, needed to remap
         - i.e., I had to do a better job marshalling
         - rely need to save state between calls
       - needed to do array indexing with the string
         buffer[count]  or buffer ++

--- conversions.s
+++ conversions.s
@@ -14,73 +14,92 @@
     .text
     .globl main
     
-# Add a macro to help out
-.macro	print_string(%reg)
-    	move $a0, %reg	# print_string(buffer)
-    	li $v0, 8
-    	syscall
+.macro  push(%reg)
+        subi $sp, $sp, 4
+        sw %reg, 0($sp)
+.end_macro
+
+.macro  pop(%reg)
+        lw %reg, 0($sp)
+        addi $sp, $sp, 4
 .end_macro
 
             
 main:
-    	# $t7: count	# note that t1 is used by a subroutine
-    	# $t8: &buffer	# pick two registers not used...
+                # $t7: count            # note that t1 is used by a subroutine
+                # $t8: &buffer          # pick two registers not used...
+        
+                li $a0, 16
+                li $v0, 9               # &buffer = malloc(16)
+                syscall
+                move $t8, $v0
+        
+                move $a0, $t8           # count = percent_base_10(&buffer, size, 1234)
+                li $a1, 16
+                li $a2, 1234    
+                jal percent_base_10
+                move $t7, $v0           
+                
+                move $a0, $t8           # print_string(buffer)
+                li $v0, 4
+                syscall
+                
+                li $a0, 0               # exit(0)
+                li $v0, 17
+                syscall
+        
+        
+percent_base_10:  nop                   # percent_base_10(&buffer, size, value)
+                                        # prints the value in ASCII on stdout
+                # v0: count
+                # a0: &buffer
+                # a1: size
+                # a2: value
+                # t0: count
+                # t1: value
+                # t2: digit
+                # t3: 10
+                # t4: size
+                # s0: &buffer           # I did not want to re-adjust all my previous
+                # s1: size              # register allocation, so I just place the new
+                # s2: buff[count]       # variables in the "s" registers
+        
+                # Copy the values from the "a" registers
+                move $s0, $a0
+                move $s1, $a1
+                move $t1, $a2   
+                li $t3, 10              # place the immediate value in register
+        
+                li $t0, 0               ## count = 0;
 
-    	li $a0, 16
-    	li $v0, 9 		# &buffer = malloc(16)
-    	syscall
-    	move $t8, $v0
+   top1:        nop                     ## do {
+                divu $t1, $t3           ##
+                mfhi $t2                ##   digit = value % base;
+                mflo $t1                ##   value = value / base;
+                push($t2)               ##   push($t2)
+                addi $t0, $t0, 1        ##   count++;
+        
+                bne $t1, $zero, top1    ## } while (value != 0);
+        
+                move $t4, $t0           ## size = count;
 
-	move $a0, $t8	# count = percent_base_10(&buffer, size, 1234)
-	li $a1, 16
-    	li $a2, 1234	
-    	jal percent_base_10
-    	move $t7, $v0 		
-	
-    	move $a0, $t8	# print_string(buffer)
-    	li $v0, 4
-    	syscall
-	
-    	li $a0, 0 	    	# exit(0)
-    	li $v0, 17
-    	syscall
+                li $t0, 0               ## count = 0;
+    top2:       nop                     ## do {
+                pop($t2)                ##   pop(digit);
 
+                                        ##   buffer[count] = digit2ascii(digit)
+                add $t2, $t2, '0'            # digit = digit + '0' // transform it to ascii
+                add $s2, $s0, $t0            # location = buffer + count
+                sb $t2, 0($s2)          ##   # mem[location] = digit
+  
+                addi $t0, $t0, 1        ##   count++;
+                subi $t4, $t4, 1        ##   size--;
+        
+                bgt $t4, $zero, top2    ## } while (size > 0 );
 
-percent_base_10:  nop   	# percent_base_10(&buffer, size, value)
-			# prints the value in ASCII on stdout
-    	# v0: count
-    	# a0: &buffer
-    	# a1: size
-    	# a2: value
-    	# t0: count
-    	# t1: value
-    	# t2: digit
-    	# t3: 10
-    	# t4: size
-    	# s0: &buffer	# I did not want to re-adjust all my previous
-    	# s1: size		# register allocation, so I just place the new
-    	# s2: buff[count]	# variables in the "s" registers
+                                        ## buffer[count] = '\0'   //add the null char
+                add $s2, $s0, $t0          # location = buffer + count
+                sb $zero, 0($s2)           # mem[location] = 0
+                move $v0, $t4           ## return count;
+                jr $ra
 
-	# Copy the values from the "a" registers
-	move $s0, $a0
-	move $s1, $a1
-	move $t1, $a2	
-	li $t3, 10 		# place the immediate value in register
-
-	li $t0, 0		## count = 0;
-   top1:	nop		## do {
-    	divu $t1, $t3 	##
-    	mfhi $t2		##   digit = value % base;
-    	mflo $t1 		##   value = value / base;
-
-			##   buffer[count] = digit2ascii(digit)
-    	add $t2, $t2, '0'	     # digit = digit + '0' // transform it to ascii
-    	add $s2, $s0, $t0            # location = buffer + count
-    	sb $t2, 0($s2)          ##   # mem[location] = digit
-
-	addi $t0, $t0, 1	##   count++;
-
-	bne $t1, $zero, top1    ## } while (value != 0);
-
-	move $v0, $t0	## return count;
-    	jr $ra
